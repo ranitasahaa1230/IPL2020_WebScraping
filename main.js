d5812dcb536ebfe711167d010de3247ed1659411
Ranita Saha <saharanitaa1230@gmail.com>
2021-03-28 11:53:41 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const { fs, path, cheerio, creareDir, request } = require("./includes");
+const allMatches = require("./modules/allMatches");
+const makeTeams = require("./modules/makeTeams");
+
+
+
+creareDir("IPL 2020")
+
+const teams__url = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/points-table-standings"
+makeTeams(teams__url)
+
+
+let all__matches = "https://www.espncricinfo.com/series/ipl-2020-21-1210595/match-results"
+console.log("before");
+allMatches(all__matches)
+console.log("after");
